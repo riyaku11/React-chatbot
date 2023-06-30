a431d51541ae5caaa10904669fbdd75fc465dd4d
riyaku11 <riyaku217@gmail.com>
2023-06-30 20:35:08 +0530

    k

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css"
+
+function App() {
+  return (
+    <div className="bg-black">
+
+    <h1 className=" text-4xl font-bold underline">
+      Hello world!
+    </h1>
+
+    </div>
+  );
+}
+
+export default App;
